4295f5e453d99f4c6405b6307ceee6ad30adddde
cryptolake <dhiadah@gmail.com>
2021-09-30 19:42:28 +0100

    hmm would 0 expected output work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, "", 0);
+	write(2, "fail", 0);
 	write(STDOUT_FILENO, q, 58);
 	return (1);
 }
