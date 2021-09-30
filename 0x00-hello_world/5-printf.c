20d97a704d74bfb0fb3e6eb821be72acc64f9700
cryptolake <dhiadah@gmail.com>
2021-09-30 13:21:30 +0100

    printf > puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints out with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
