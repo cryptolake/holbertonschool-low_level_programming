a4534a557598fd75e34787c7aac458127ba6b085
cryptolake <dhiadah@gmail.com>
2021-09-30 13:09:30 +0100

    Documentation is important

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+* main - Prints out "Programming is like building a multilingual puzzle
+* Return: nothing
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\0\n");
