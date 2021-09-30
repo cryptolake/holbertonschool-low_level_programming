fe2149598310e5591d24f89a52f48563010fa5c1
cryptolake <dhiadah@gmail.com>
2021-09-30 19:30:16 +0100

    how do you write to stderr?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
 */
 int main(void)
 {
-	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(1, q, 58);
-	write(2, "1", 1);
+	write(STDOUT_FILENO, q, 58);
+	write(STDERR_FILENO, "FAILURE", 7);
 	return (0);
 }
