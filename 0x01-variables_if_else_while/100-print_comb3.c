6eb33d246ca6e9d40d0ced030ce66ab8fd473545
cryptolake <dhiadah@gmail.com>
2021-10-01 16:16:05 +0100

    combination can stack up

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i,j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i+1; j <= 57; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar(j);
+				if (!( i == 56 && j == 57 ))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
