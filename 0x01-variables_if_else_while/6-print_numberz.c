aed4e9accce4d34d472cf444bfd2f6e4d8dcf17e
cryptolake <dhiadah@gmail.com>
2021-10-01 12:20:29 +0100

    digits and digits AND DIGITS

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digits
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
