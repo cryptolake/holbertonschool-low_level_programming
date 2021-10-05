1d78a8fd38458ce6c2f200e83fa3578c0aeb21ae
cryptolake <dhiadah@gmail.com>
2021-10-05 10:18:59 +0100

    betty and my first function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0
+ **/
 int main(void)
 {
 	printf("_putchar\n");
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: 0
+ **/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 95; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("/n");
+}
