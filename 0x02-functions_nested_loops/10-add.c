d326b3cd1ca51691f9dc28e55e2df409926b0511
cryptolake <dhiadah@gmail.com>
2021-10-05 13:34:30 +0100

    addtion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @n: first number
+ * @p: second number
+ *
+ * Return: result of addition
+ **/
+int add(int n, int p)
+{
+	return (n + p);
+}
