6db7d01491e53abffd68310d210cad3e11e059d5
cryptolake <dhiadah@gmail.com>
2021-10-06 09:56:46 +0100

    betty and refactor

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * sum_three_five - get sum of multiples of 3 or 5
+ * @k: starting point
+ * @l: limit
+ *
+ * Return: sum of multiples
+ **/
+int sum_three_five(int k, int l)
 {
-	int a,i;
+	int a, i;
 
 	a = 0;
-	for (i = 3; i < 1024; i++)
+	for (i = k; i < l; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			a += i;
 		}
 	}
-	printf("%d\n",a);
+	return (a);
+}
+
+/**
+ * main - print the sum
+ *
+ * Return: (0) success
+ **/
+int main(void)
+{
+	printf("%d\n",sum_three_five(3,1024));
+
 	return (0);
+
 }
