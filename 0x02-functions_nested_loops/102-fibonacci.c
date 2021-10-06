f664b25152144c7447db4e40575b67218fb1e8af
cryptolake <dhiadah@gmail.com>
2021-10-06 10:39:14 +0100

    fibonacci is bb

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void fib(int l)
+{
+	int x;
+	long long int f[100];
+
+	f[0] = 0;
+	f[1] = 1;
+
+	for (x = 2; x <= l; x++)
+	{
+		f[x] = f[x - 1] + f[x - 2];
+		printf("%lld, ",f[x]);
+	}
+}
+
+int main(void)
+{
+	fib(50);
+	printf("\n");
+}
