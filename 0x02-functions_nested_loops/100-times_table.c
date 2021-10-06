2a591c47fb30ba32f3b464498018765364fe2cfc
cryptolake <dhiadah@gmail.com>
2021-10-06 10:52:38 +0100

    betty + less lines in times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,11 +42,9 @@ void print_times_table(int n)
 						_putchar(' ');
 					}
 				}
-
 				_putchar(x % 10 + '0');
 			}
 			_putchar('\n');
 		}
-
 	}
 }
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * fib - print the l terms of the fibonacci sequence
+ *
+ * @l: number of terms
+ *
+ * return: nothing
+ **/
 void fib(int l)
 {
 	int x;
@@ -8,21 +15,27 @@ void fib(int l)
 	f[0] = 0;
 	f[1] = 1;
 
-	for (x = 2; x <= l+1; x++)
+	for (x = 2; x <= l + 1; x++)
 	{
 		f[x] = f[x - 1] + f[x - 2];
 
-		if (x == l+1)
+		if (x == l + 1)
 		{
-			printf("%ld",f[x]);
+			printf("%ld", f[x]);
 		}
 		else
 		{
-			printf("%ld, ",f[x]);
+			printf("%ld, ", f[x]);
 		}
 	}
 }
 
+/**
+ * main - add newline
+ *
+ *
+ * Return: 0
+ **/
 int main(void)
 {
 	fib(50);
