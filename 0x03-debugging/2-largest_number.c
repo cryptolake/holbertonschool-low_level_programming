df12c30a274714ed785d22dfabb285b9bd10911f
cryptolake <dhiadah@gmail.com>
2021-10-07 09:32:42 +0100

    testing and trying

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,21 @@
 int largest_number(int a, int b, int c)
 {
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		return (a);
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		return (b);
 	}
-	else if (c > b && c > a)
+	else if (c >= b && c >= a)
 	{
 		return (c);
 	}
 	else
 	{
-		return (0);
+		return (c);
 	}
 
 }
