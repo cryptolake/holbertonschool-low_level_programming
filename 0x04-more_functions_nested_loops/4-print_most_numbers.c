ec3033b824971d2f1a44884eb3aa0867364e6f1d
cryptolake <dhiadah@gmail.com>
2021-10-07 13:15:55 +0100

    almost all

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all digits except 2 and 4
+ *
+ * Return: nothing
+ **/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '4' && i != '2')
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
