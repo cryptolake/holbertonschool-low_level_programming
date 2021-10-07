02b57e67cf552ae237a3d0df0e517da176bd1061
cryptolake <dhiadah@gmail.com>
2021-10-07 17:01:19 +0100

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void print_triangle(int size)
+{
+	int i, j, l;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j <= size - i - 1; j++)
+				_putchar(' ');
+
+			for (l = 1; l <= i; l++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
