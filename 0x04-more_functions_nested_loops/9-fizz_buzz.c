a827c56e01830d19236a67341fabe5e3ac78f292
cryptolake <dhiadah@gmail.com>
2021-10-07 16:37:02 +0100

    refactor

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,13 @@
 void fizzbuzz(int i)
 {
 	if (i % 5 == 0 && i % 3 == 0)
-		printf("FizzBuzz ");
-	else if (i % 3 == 0)
-		printf("Fizz ");
-	else if (i % 5 == 0)
-		printf("Buzz ");
+		printf("FizzBuzz");
+	else if (i % 3 == 0 && i % 5 != 0)
+		printf("Fizz");
+	else if (i % 5 == 0 && i % 3 != 0)
+		printf("Buzz");
 	else
-		printf("%d ", i);
+		printf("%d", i);
 }
 
 /**
@@ -30,6 +30,10 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		fizzbuzz(i);
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
