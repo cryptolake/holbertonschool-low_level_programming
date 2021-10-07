93dd31efa0a38a0ee34611a078ecd155a9a90f52
cryptolake <dhiadah@gmail.com>
2021-10-07 19:13:18 +0100

    primes are prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,49 @@
+#include <math.h>
+#include <stdio.h>
+
+
+/**
+ * isprime - test if the number is prime or not
+ * @x: number to test
+ *
+ * Return: 1 prime 0 not prime
+ **/
+int isprime(int x)
+{
+	int i;
+
+	if (x == 2)
+	{
+		return (1);
+	}
+
+	for (i = 2; i <= sqrt(x); i++)
+	{
+		if (x % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+
+/**
+ * main - get the biggest prime factor of
+ * 612852475143.
+ *
+ * Return: always 0
+ **/
+int main(void)
+{
+	int i, x;
+
+	for (i = 2; i <= sqrt(612852475143); i++)
+	{
+		if (isprime(i) && i >= x)
+			x = i;
+	}
+	printf("%d\n", x);
+
+	return (0);
+}
