b93249d5661f6437777c1b725a6d866aa52a409f
cryptolake <dhiadah@gmail.com>
2021-10-09 19:25:34 +0100

    refactor

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,56 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 void print_number(int n)
 {
 	int x, y, r;
 
 	x = 0;
-	y = n;
 
-	if (n > 0)
-	{
-		while (y != 0)
-		{
-			r = y % 10;
-			x = x * 10 + r;
-			y = y / 10;
-		}
-
-		while (x > 0)
-		{
-			_putchar((x % 10) + '0');
-			x = x / 10;
-		}
-		if (n % 10 == 0)
-		{
-			_putchar('0');
-		}
-	}
-	else if (y < 0)
-	{
-		y = -y;
-
-		_putchar('-');
-
-		while (y != 0)
-		{
-			r = y % 10;
-			x = x * 10 + r;
-			y = y / 10;
-		}
-		while (x > 0)
-		{
-			_putchar((x % 10) + '0');
-			x = x / 10;
-		}
-		if (n % 10 == 0)
-		{
-			_putchar('0');
-		}
-	}
+	if (n < 0)
+		y = -n;
 	else
+		y = n;
+
+	if (n == 0)
 	{
 		_putchar('0');
+	}
 
+	while (y != 0)
+	{
+		r = y % 10;
+		x = x * 10 + r;
+		y = y / 10;
+	}
+	if (n < 0)
+		_putchar('-');
+
+	while (x > 0)
+	{
+		_putchar((x % 10) + '0');
+		x = x / 10;
+	}
+	if (n % 10 == 0)
+	{
+		_putchar('0');
 	}
 }
