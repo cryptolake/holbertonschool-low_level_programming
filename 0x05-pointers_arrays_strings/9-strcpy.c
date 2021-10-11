1bb5b45c7186e65b6ffab1b708dca4e8b199232d
cryptolake <dhiadah@gmail.com>
2021-10-11 18:23:47 +0100

    copy copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string
+ *
+ * @s: string
+ *
+ * Return: length
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ *
+ *
+ *
+ **/
+char *_strcpy(char *dest, char *src)
+{
+	int l, i;
+
+	l = _strlen(src);
+
+	for (i = 0; i <= l; i++)
+	{
+		dest[i] = src[i];
+	}
+
+
+	return src;
+}
