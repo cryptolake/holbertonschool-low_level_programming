54244e1e9560d92884b6a92615ba2e25eae71a69
cryptolake <dhiadah@gmail.com>
2021-10-11 13:46:46 +0100

    reversing things

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,19 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (s[i+1] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+
+	if (i > 0)
 	{
-		_putchar(s[i]);
 		i--;
+		while (i >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
