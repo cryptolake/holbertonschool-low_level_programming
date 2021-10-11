15708e33c7afae7ab01ec8b594df88bb1cc69f9f
cryptolake <dhiadah@gmail.com>
2021-10-11 15:04:46 +0100

    reversing the string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - get length of string
+ *
+ * @s: string
+ *
+ * Return: length
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: string
+ **/
+void rev_string(char *s)
+{
+	int i;
+	int	j;
+	char str[100];
+
+	i = _strlen(s) - 1;
+	j = 0;
+
+	while (i >= 0)
+	{
+		str[j] = s[i];
+		j++;
+		i--;
+	}
+	str[j] = '\0';
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+}
