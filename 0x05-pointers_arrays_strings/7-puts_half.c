c77150188f439d7f74aed979155232998d6c8184
cryptolake <dhiadah@gmail.com>
2021-10-11 15:59:20 +0100

    half half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_char - print the second half of string
+ * @str: string
+ *
+ **/
+void puts_half(char *str)
+{
+	int i, n, l;
+
+	l = strlen(str);
+
+	if (l % 2 == 0)
+	{
+		n = l / 2;
+	}
+	else
+	{
+		n = (l - 1) / 2;
+	}
+
+	for (i = n; i < l; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
