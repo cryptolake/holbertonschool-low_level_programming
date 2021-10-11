116cfad66c56259f1ad2bd5ee96c5a8b2f41290a
cryptolake <dhiadah@gmail.com>
2021-10-11 12:58:07 +0100

    swapping action

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two variables
+ *
+ * @a: first var
+ * @b: second var
+ **/
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*b = c;
+	*a = *b;
+}
