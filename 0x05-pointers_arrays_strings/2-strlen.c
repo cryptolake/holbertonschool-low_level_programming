d625be2f1036ea6d3801b1fa7e03d35342d4b9af
cryptolake <dhiadah@gmail.com>
2021-10-11 13:09:47 +0100

    betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,17 @@
  * _strlen - get length of string
  *
  * @s: string
+ *
+ * Return: length
  **/
 int _strlen(char *s)
 {
 	int i;
 
-	i=0;
-	while(*(s + i) != '\0')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-	return i;
+	return (i);
 }
