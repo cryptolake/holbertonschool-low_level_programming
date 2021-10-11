411968080574d796213d3ebf1351d0f9ed40cf71
cryptolake <dhiadah@gmail.com>
2021-10-11 15:27:36 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+void puts2(char *str)
+{
+	int i,j;
+
+	i = strlen(str);
+
+	for (j = 0; j <= i - 1; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+
+}
