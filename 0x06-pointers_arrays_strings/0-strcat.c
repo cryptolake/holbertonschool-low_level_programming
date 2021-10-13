601173dec2ab726b10f9d8739811c2afae2fd010
cryptolake <dhiadah@gmail.com>
2021-10-13 13:42:16 +0100

    conCAT

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+char *_strcat(char *dest, char *src)
+{
+	int ls, ld , i, j, l;
+	ls = strlen(src); 
+	ld = strlen(dest);
+	l = ls + ld;
+	j = 0;
+
+	for (i = ld; i < l-1; i++)
+	{
+		dest[i] = src[j]; 
+		j++;
+	}
+	dest[l] = '\0';
+
+
+	return dest;
+}
