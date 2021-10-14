a839403563f4a5ca8464acc0e19404d60308bd19
cryptolake <dhiadah@gmail.com>
2021-10-14 11:26:27 +0100

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strncpy - copy n bytes of string
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ *
+ * Return: pointer to dest
+ **/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
 
 	for (i = 0; i < n; i++)
 	{
