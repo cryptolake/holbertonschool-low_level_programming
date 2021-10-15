283aac47c311f6aa2c4f739b780478788cb7c8d3
cryptolake <dhiadah@gmail.com>
2021-10-15 20:42:27 +0100

    printing number the string way

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_number(int n) {
+  int x, i, y;
+  char s[50];
+
+  y = 1;
+  x = 0;
+  if (n != 0)
+  {
+	if (n < 0)
+	{
+	  _putchar('-');
+	  y = -1;
+	}
+
+	while (n != 0)
+	{
+	  s[x] = ((y * n) % 10) + '0';
+	  n /= 10;
+	  x++;
+	}
+	for (i = x - 1; i >= 0; i--)
+	{
+	  _putchar(s[i]);
+	}
+  }
+  else
+  {
+	  _putchar('0');
+  }
+}
