0d073153603843b3feb76dfe242053e57dd00076
cryptolake <dhiadah@gmail.com>
2021-10-16 12:45:25 +0100

    betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * count_d10 - count number of digits
+ *
+ * @n: number
+ *
+ * Return: 10^count
+ **/
 int count_d10(int n)
 {
 	int x;
+
 	x = 1;
 	n /= 10;
 	while (n != 0)
@@ -10,9 +18,14 @@ int count_d10(int n)
 		n /= 10;
 		x *= 10;
 	}
-	return x;
+	return (x);
 }
 
+/**
+ * print_number - print a number
+ *
+ * @n: number
+ **/
 void print_number(int n)
 {
 	int x, y, c;
@@ -27,7 +40,7 @@ void print_number(int n)
 
 	while (x != 0)
 	{
-		c = y * ((n / x) % 10) + '0' ;
+		c = y * ((n / x) % 10) + '0';
 		x /= 10;
 		_putchar(c);
 	}
