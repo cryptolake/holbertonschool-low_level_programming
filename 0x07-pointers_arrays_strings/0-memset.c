35e1ab4f137210fe2d8b1d2b1375c72ad1dc25e0
cryptolake <dhiadah@gmail.com>
2021-10-19 09:08:51 +0100

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,18 +2,19 @@
 
 /**
  * _memset -  fills the first n bytes of the memory area with a constant byte
- * @s: memory addess
+ * @s: memory address
  * @b: byte
  * @n: length of area
  *
- * Return: s
+ * Return: address in s
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
-	return s;
+	return (s);
 }
