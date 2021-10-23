03148b512f18e96a55f29364b964f6a004669a43
cryptolake <dhiadah@gmail.com>
2021-10-23 14:39:19 +0100

    head is important

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * calc_coin - calculate the minimum coin
  * number of m amount.
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int _putchar(char c);
 int check(int len, char *s[]);
