81196ddf0e1c23ed3d0e896db11e5273e849dfaa
cryptolake <dhiadah@gmail.com>
2021-10-24 16:17:36 +0100

    recursive palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * rec_strncpy - recursive strncpy
+ *
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ *
+ **/
+void rec_strncpy(char *dest, char *src, int n)
+{
+	if (n >= 0)
+	{
+		dest[n] = src[n];
+		rec_strncpy(dest, src, n - 1);
+	}
+}
+
+/**
+ * is_palindrome - check if string is
+ * palindrom or not.
+ *
+ * @s: string
+ *
+ * Return: 1 palindrome, 0 not
+ **/
+int is_palindrome(char *s)
+{
+	int l;
+	char st[100];
+
+	l = strlen(s) - 1;
+
+	if (l <= 2 && s[0] == s[l])
+	{
+		return (1);
+	}
+	else if (s[0] == s[l])
+	{
+		rec_strncpy(st, s, l);
+		st[l] = '\0';
+		return (is_palindrome(st + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
