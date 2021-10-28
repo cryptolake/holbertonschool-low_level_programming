a6e199e33f96e120720b277200a7d95f850d321e
cryptolake <dhiadah@gmail.com>
2021-10-28 21:20:26 +0100

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * string_nconcat - concatenates two strings with
  * specified bytes
- *
+ * 
  * @s1: string
  * @s2: string
  * @n: bytes
@@ -14,42 +14,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int l, l1, l2, i, j;
+	unsigned int l, l1, l2, i;
 
 
-	if (s1 == NULL)
-		l1 = 0;
-	else
-		l1 = strlen(s1);
+	if (!s1)
+		s1 = "";
 
-	if (s2 == NULL)
-		l2 = 0;
-	else
-		l2 = strlen(s2);
+	if (!s2)
+		s2 = "";
 
-	if (n > l2)
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
+	if (n >= l2)
 		n = l2;
 
-	l = l1 + n + 1;
+	l = l1 + n;
 
-	s = malloc(l);
-	if (s == NULL)
+	s = malloc(l + 1);
+	if (!s)
 		return (NULL);
 
-	i = 0;
-	while (i < l1)
+	for (i = 0; i < l; i++)
 	{
-		s[i] = s1[i];
-		i++;
+		if (i < l1)
+			s[i] = s1[i];
+		else
+			s[i] = s2[i - l1];
 	}
-	j = 0;
-	while (j < n)
-	{
-		s[i] = s2[j];
-		i++;
-		j++;
-	}
-	s[i] = '\0';
+	s[l] = '\0';
+
 
 	return (s);
 }
