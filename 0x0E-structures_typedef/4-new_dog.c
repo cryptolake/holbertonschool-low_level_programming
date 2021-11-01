f2537bb08f1e9ead36463a7a1c48ebc79df83c4a
cryptolake <dhiadah@gmail.com>
2021-11-01 14:50:52 +0100

    more testing for dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-
 	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
+
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
