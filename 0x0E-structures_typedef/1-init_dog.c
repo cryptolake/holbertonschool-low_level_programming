fb5bade9b68094172de3d4eacea931dbaaa632de
cryptolake <dhiadah@gmail.com>
2021-11-01 13:43:15 +0100

    betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,11 @@
 #include "dog.h"
 
+/**
+ * init_dog - initialize the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
