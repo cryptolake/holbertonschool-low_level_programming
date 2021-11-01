67e44ac6dae49a77daacb54cbe4753572ea9f0fe
cryptolake <dhiadah@gmail.com>
2021-11-01 12:18:33 +0100

    betty lint

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int main()
+/**
+ * main - print file name
+ *
+ * Return: always (0)
+ **/
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return 0;
+	return (0);
 }
