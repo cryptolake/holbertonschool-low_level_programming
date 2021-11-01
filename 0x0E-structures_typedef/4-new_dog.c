43e39aaa2107ccd3d59a5984d21ccdeddeb13352
cryptolake <dhiadah@gmail.com>
2021-11-01 14:41:01 +0100

    a new dog is coming home

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,25 @@
 #include "dog.h"
 
+/**
+ * new_dog - creating a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Return: pointer to dog
+ **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 
 	dog = malloc(sizeof(dog_t));
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
 
-	return dog;
+	return (dog);
 }
