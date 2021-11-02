114422127f6e9fd195354af978bdeb81a5171579
cryptolake <dhiadah@gmail.com>
2021-11-02 12:49:57 +0100

    omg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
 
+
+/**
+ * free_dog - FREE THE DOG
+ * @d: dog
+ *
+ **/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
+
 /**
  * new_dog - creating a dog
  * @name: dog name
@@ -18,18 +35,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
+		free_dog(dog);
 		return (NULL);
-
+	}
 	dog->name = strdup(name);
 	if (dog->name == NULL)
 	{
-		free(dog);
+		free_dog(dog);
 		return (NULL);
 	}
 	dog->owner = strdup(owner);
 	if (dog->owner == NULL)
 	{
-		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
