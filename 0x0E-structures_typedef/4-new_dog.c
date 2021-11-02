efe4a4032cb4ab72f8289b0f81777ccadee9d811
cryptolake <dhiadah@gmail.com>
2021-11-02 12:42:07 +0100

    testiiiiiiiiiing

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,35 +12,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *dname;
-	char *downer;
 
 	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
 
-
-	dname = strdup(name);
-	if (dname == NULL)
-		return (NULL);
-
-	downer = strdup(owner);
-	if (downer == NULL)
-	{
-		free(dname);
-		return (NULL);
-	}
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+		return (NULL);
+
+	dog->name = strdup(name);
+	if (dog->name == NULL)
+		return (NULL);
+
+	dog->owner = strdup(owner);
+	if (dog->owner == NULL)
 	{
-		free(dname);
-		free(downer);
+		free(dog->owner);
 		return (NULL);
 	}
 
-	dog->name = dname;
+
 	dog->age = age;
-	dog->owner = downer;
 
 	return (dog);
 }
