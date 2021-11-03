be54197b9259ca7f644e446b5a0660d3eccc48cf
cryptolake <dhiadah@gmail.com>
2021-11-03 11:36:36 +0100

    betty!!

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include "function_pointers.h"
 
 
+/**
+ * print_name - print name with function
+ * @name: the name
+ * @f: the function
+ *
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,17 @@
 #include "function_pointers.h"
 
 
+/**
+ * array_iterator - do action on every element of array
+ * @array: the array
+ * @size: size of the array
+ * @action: the action to perform
+ *
+ **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 
