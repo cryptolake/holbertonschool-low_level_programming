20714e06ed65764b280e9804e7a0b4fb8b924b3f
cryptolake <dhiadah@gmail.com>
2021-11-03 11:28:57 +0100

    This reminds of js 🤔

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H_
 #define FUNCTION_POINTERS_H_
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
