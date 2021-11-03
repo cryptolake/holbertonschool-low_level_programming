a3391e21fe89822061b1b2c6ab807fae0bda0c31
cryptolake <dhiadah@gmail.com>
2021-11-03 15:53:34 +0100

    THE ULTIMATE CALCULATOR

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (strlen(op) != 1 && (op[0] != '+' || op[0] != '-' ||
+		op[0] != '*' || op[0] != '/' ||
+		op[0] != '%'))
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+
+	if ((op[0] == '/' || op[0] == '%') && num2 == 0)
+	{
+		printf("ERROR\n");
+		exit(100);
+
+	}
+
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
