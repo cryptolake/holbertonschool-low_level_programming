b69cc52a3e9654b069732b4605e1dadf0d5f37d0
cryptolake <dhiadah@gmail.com>
2021-11-03 12:16:03 +0100

    well testing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <string.h>
 
 
 /**
@@ -9,5 +10,6 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL && strlen(name) > 0)
+		f(name);
 }
