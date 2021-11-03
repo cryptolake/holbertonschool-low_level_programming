52b3e89ce6ad022ebdb90276b7b7ee8b1f598cef
cryptolake <dhiadah@gmail.com>
2021-11-03 12:05:03 +0100

    this pretty amazing actually

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - find element of array that matches
+ * cmp
+ * @array: the array
+ * @size: its size
+ * @cmp: function to compare against
+ * Return: index of the element otherwise (-1)
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+
+
+	return (-1);
+}
