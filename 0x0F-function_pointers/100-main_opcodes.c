098ed8d064b99052f0f77315618cb2b222d7be94
cryptolake <dhiadah@gmail.com>
2021-11-04 09:05:01 +0100

    OPCODES

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print opcodes of itself
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ *
+ * Return: 0
+ *
+ **/
+int main(int argc, char *argv[])
+{
+
+	unsigned char *ptr;
+	int i, n;
+
+	ptr = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%x ", *(ptr + i));
+
+	}
+		printf("\n");
+
+	return (0);
+}
