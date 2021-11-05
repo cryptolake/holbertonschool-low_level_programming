a379dc9bbb3c8393ab5479ad85ace824b3fe05f8
cryptolake <dhiadah@gmail.com>
2021-11-05 23:17:48 +0100

    this should work without null handle :(

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,53 +2,84 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * pchr - print char arg
+ * @ap: arg
+ *
+ **/
 void pchr(va_list ap)
 {
 	printf("%c", va_arg(ap, int));
 
 }
+
+/**
+ * pstr - print string arg
+ * @ap: arg
+ *
+ **/
 void pstr(va_list ap)
 {
 	char *s;
 
 	s = va_arg(ap, char *);
-
 	printf("%s", s);
 }
 
+/**
+ * pint - print int arg
+ * @ap: arg
+ *
+ **/
 void pint(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
 
+/**
+ * pflt - print float arg
+ * @ap: arg
+ *
+ **/
 void pflt(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
 
+/**
+ * print_all - function that prints anything
+ *
+ * @format: format string
+ **/
 void print_all(const char * const format, ...)
 {
 	va_list ap;
 	int i = 0, j;
-	parse types[] = {{'c', pchr},{'s', pstr},{'i', pint},{'f', pflt}};
+	int x = 0;
+	parse types[] = {{'c', pchr}, {'s', pstr},
+					{'i', pint}, {'f', pflt}, {0, NULL}};
 
 	va_start(ap, format);
-	while (format[i] != '\0')
+	while (format[i])
 	{
 		j = 0;
-		while (j < 4)
+		if (x)
+			printf(", ");
+
+		x = 0;
+		while (types[j].chr)
 		{
-			if (format[i] == types[j].chr)
+			if (types[j].chr == format[i])
 			{
-				if (i > 0)
-					printf(", ");
 				types[j].f(ap);
-				break;
+				x = 1;
 			}
 			j++;
 		}
+
 		i++;
 	}
 
-	printf("\n");
+	putchar('\n');
+	va_end(ap);
 }
