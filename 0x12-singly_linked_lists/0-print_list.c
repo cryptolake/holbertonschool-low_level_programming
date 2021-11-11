aa8c3f41a2d90b96929d2771042cd3062564a6da
cryptolake <dhiadah@gmail.com>
2021-11-11 20:17:51 +0100

    fixed length of NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 size_t print_list(const list_t *h)
 {
 	if (h->str == NULL)
-		printf("[%d] %s\n", h->len, "(nil)");
+		printf("[%d] %s\n", 0, "(nil)");
 	else
 		printf("[%d] %s\n", h->len, h->str);
 
