fd77380aa28132e81ee1d99eb07963ff314915c4
cryptolake <dhiadah@gmail.com>
2021-11-11 23:07:41 +0100

    execute this before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+/**
+ * thefirst - execute before main
+ **/
 void __attribute__ ((constructor)) thefirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
