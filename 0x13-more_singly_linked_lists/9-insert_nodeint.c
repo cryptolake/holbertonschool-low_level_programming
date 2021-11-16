7a1bfd4a463b6df3698abab672c6cda84eb4c81f
cryptolake <dhiadah@gmail.com>
2021-11-16 20:00:24 +0100

    Fixed some edge cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,14 +16,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!*head && idx >= 1)
 		return (NULL);
-	else if (!*head && idx == 0)
+	else if (idx == 0)
 	{
 		new = malloc(sizeof(listint_t));
 		if (!new)
 			return (NULL);
 
 		new->n = n;
-		new->next = NULL;
+		new->next = *head;
 		*head = new;
 
 		return (new);
