70f7346e66ff3b929f87af11bc80b04b4d94434b
cryptolake <dhiadah@gmail.com>
2021-11-18 18:46:03 +0100

    more checks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,8 +9,11 @@
  **/
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index != 0 && n == 0)
+		return (-1);
+
 	if (index == 0)
 		return (n & 1);
-	else
-	return get_bit(n >> 1, index - 1);
+
+	return (get_bit(n >> 1, index - 1));
 }
