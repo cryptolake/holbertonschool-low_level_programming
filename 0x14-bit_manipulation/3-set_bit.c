6e76782849c5bf02520b3fb2ca3a6fc21fd36704
cryptolake <dhiadah@gmail.com>
2021-11-18 22:13:18 +0100

    error checking

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * countbit - count the bit length
+ * @n: number
+ * Return: number of bits
+ **/
+unsigned int countbit(unsigned int n)
+{
+	unsigned int count = 0;
+
+	if (n == 0)
+		return (1);
+
+	while (n)
+	{
+		count++;
+		n >>= 1;
+	}
+	return (count);
+}
+
+/**
  * set_bit - set bit at index to 1
  * @n: number
  * @index: index
@@ -9,6 +29,9 @@
  **/
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > countbit(*n) - 1)
+		return (-1);
+
 	*n |= 1 << index;
 
 	return (1);
