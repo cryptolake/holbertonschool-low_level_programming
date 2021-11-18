7416b4b211af0016027bb997b3dcc881380d39ac
cryptolake <dhiadah@gmail.com>
2021-11-18 22:10:38 +0100

    Fixed error checking

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,6 +2,26 @@
 #include <stdio.h>
 
 /**
+ * countbit - count the bit length
+ * @n: number
+ * Return: number of bits
+ **/
+unsigned int countbit(unsigned int n)
+{
+	unsigned int count = 0;
+
+	if (n == 0)
+		return (1);
+
+	while (n)
+	{
+		count++;
+		n >>= 1;
+	}
+	return (count);
+}
+
+/**
  * get_bit - get bit of number at position index
  * @n: number
  * @index: index
@@ -9,6 +29,8 @@
  **/
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > countbit(n) - 1)
+		return (-1);
 	n >>= index;
 	return (n & 1);
 }
