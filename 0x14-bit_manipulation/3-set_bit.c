a6aec71fae1d5be0e80a262ad7d246db902cd85f
cryptolake <dhiadah@gmail.com>
2021-11-19 08:37:38 +0100

    fixed size

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@
  **/
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index > 32)
 		return (-1);
 
 	*n |= 1 << index;
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	/* size of unsigned long int  */
-	if (index > 64)
+	if (index > 32)
 		return (-1);
 
 	*n &=  ~(1 << index);
