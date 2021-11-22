acafa49e71f8baad855f77c9b2fea92f774ccfef
cryptolake <dhiadah@gmail.com>
2021-11-22 16:38:36 +0100

    reading and ouputing content of file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t count;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename,O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	count = read(fd, buffer, letters);
+	if (count == -1)
+		return (0);
+	count = write(STDOUT_FILENO, buffer, count);
+	if (count != letters)
+		return (0);
+
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
