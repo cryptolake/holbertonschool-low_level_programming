65e4e4a56379617a9bd458560c9fcf77cc4f455e
cryptolake <dhiadah@gmail.com>
2021-11-22 19:20:13 +0100

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * create_file - create a file
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 (success) -1 (error)
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t count;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 110000000);
+	if (fd == -1)
+		return (-1);
+
+	count = write(fd, text_content, strlen(text_content));
+	if (count == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
