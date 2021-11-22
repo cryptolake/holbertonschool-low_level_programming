15a2810992d2d39ad3bc3f3b30425a2691a3b846
cryptolake <dhiadah@gmail.com>
2021-11-22 20:19:56 +0100

    fixed stinky bug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * append_text_to_file - appends text at the end of a file.
@@ -10,17 +11,19 @@
  **/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t fd, w;
+	int fd, w;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+	{
 		return (-1);
+	}
 
 	w = write(fd, text_content, strlen(text_content));
 	if (w == -1)
