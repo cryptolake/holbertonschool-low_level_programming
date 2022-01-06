fdc678e99b0e8a946d6c442a35640832cd0f03ff
cryptolake <dhiadah@gmail.com>
2022-01-06 22:55:17 +0100

    get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* get_dnodeint_at_index - get node at index
+* @head: head on list
+* @index: of node
+* Return: node
+**/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *p = head;
+
+	for (i = 0; i < index; i++)
+		p = p->next;
+
+	return (p);
+}
