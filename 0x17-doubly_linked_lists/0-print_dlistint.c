c8aec35637c1b90869dc8587b54d161a34d1e19e
cryptolake <dhiadah@gmail.com>
2022-01-06 19:27:10 +0100

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_dlistint - print elements of linked
+* list
+* @h: head
+* Return: number of elements
+**/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
