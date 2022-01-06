986cdf5960505a01a42ce913fcf8af60c914c1d2
cryptolake <dhiadah@gmail.com>
2022-01-06 23:04:00 +0100

    test

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,9 +9,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 	dlistint_t *p = head;
+	
+	if (!p)
+		return (NULL);
 
 	for (i = 0; i < index; i++)
 		p = p->next;
-
+	
 	return (p);
 }
