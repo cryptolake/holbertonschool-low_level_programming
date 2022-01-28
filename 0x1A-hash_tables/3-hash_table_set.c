e7b41c0a3c96a20d064ac1d9fde331577685183a
cryptolake <dhiadah@gmail.com>
2022-01-28 13:00:06 +0100

    python makes you lose your edge

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t **table;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return (0);
+
+	index = key_index((unsigned const char *)key, ht->size);
+	table = ht->array;
+	if (!table)
+		return (0);
+	
+	/* collision */
+	if (table[index])
+	{
+		temp = table[index];
+		while (temp->next)
+			temp = temp->next;
+		node = malloc(sizeof(hash_node_t));
+		if (!node)
+			return (0);
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = NULL;
+		temp->next = node;
+		return (1);
+	}
+
+	table[index] = malloc(sizeof(hash_node_t));
+	if (!table[index])
+		return (0);
+	table[index]->value = strdup(value);
+	table[index]->key = strdup(key);
+	table[index]->next = NULL;
+	
+	return (1);
+}
