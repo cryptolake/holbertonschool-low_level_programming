3c10e9f912bd496eafbf9a5ddac451c9101f4193
cryptolake <dhiadah@gmail.com>
2022-01-28 19:20:35 +0100

    fixed setting value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,27 @@
 #include "hash_tables.h"
 
 /**
+* node_set - set node of hash table
+* @key: key
+* @value: value
+* Return: pointer to node
+**/
+hash_node_t *node_set(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+
+	return (node);
+}
+
+/**
 * hash_table_set - adds an element to the hash table
 * @ht: hash table
 * @key: key of element
@@ -10,8 +31,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t **table;
-	hash_node_t *node, *temp;
+	hash_node_t **table, *temp;
 
 	if (!ht)
 		return (0);
@@ -24,25 +44,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* collision */
 	if (table[index])
 	{
+		if (!strcmp(key, table[index]->key))
+		{
+			free(table[index]->value);
+			table[index]->value = strdup(value);
+			return (1);
+		}
+
 		temp = table[index];
 		while (temp->next)
 			temp = temp->next;
-		node = malloc(sizeof(hash_node_t));
-		if (!node)
+		temp->next = node_set(key, value);
+		if (!temp->next)
 			return (0);
-		node->key = strdup(key);
-		node->value = strdup(value);
-		node->next = NULL;
-		temp->next = node;
+
 		return (1);
 	}
 
-	table[index] = malloc(sizeof(hash_node_t));
+	table[index] = node_set(key, value);
 	if (!table[index])
 		return (0);
-	table[index]->value = strdup(value);
-	table[index]->key = strdup(key);
-	table[index]->next = NULL;
 
 	return (1);
 }
