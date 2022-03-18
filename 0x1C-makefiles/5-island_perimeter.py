e6679a0f5d114a1a435f19d79181a160df17e10b
cryptolake <dhiadah@gmail.com>
2022-03-18 13:23:27 +0100

    fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,22 +3,22 @@
 
 
 def check_grid(grid, i, j):
-    """Check neighbours."""
-    neigh = 0
+    """Check for neighbours or nothing."""
+    side = 0
 
-    if grid[i][j - 1] == 1:
-        neigh += 1
+    if j == 0 or grid[i][j - 1] == 1:
+        side += 1
 
-    if grid[i][j + 1] == 1:
-        neigh += 1
+    if len(grid[i]) == j + 1 or grid[i][j + 1] == 1:
+        side += 1
 
-    if grid[i - 1][j] == 1:
-        neigh += 1
+    if i == 0 or grid[i - 1][j] == 1:
+        side += 1
 
-    if grid[i + 1][j] == 1:
-        neigh += 1
+    if len(grid) == i + 1 or grid[i + 1][j] == 1:
+        side += 1
 
-    return neigh
+    return side
 
 
 def island_perimeter(grid):
