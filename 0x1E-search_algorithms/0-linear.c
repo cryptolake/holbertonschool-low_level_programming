dd2a39f930cdf75334947ebca0e1e40628ff2380
cryptolake <dhiadah@gmail.com>
2022-05-03 15:20:36 +0100

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search arrya with linear search
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ * Return: first index of value
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
